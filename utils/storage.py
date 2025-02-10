295a53c459e004e4e3f2a1f3f0e4ad593b62f7ad
Rooam Lee <leewr9@naver.com>
2025-02-10 14:45:21 +0900

    storage:: fix quotes

--- utils/storage.py
+++ utils/storage.py
@@ -15,20 +15,20 @@ def read_scraped_data(filename):
         if not os.path.exists(file_path):
             file_path = os.path.join(OUTPUT_DIR, filename)
 
-        if file_extension == '.parquet':
+        if file_extension == ".parquet":
             df = pd.read_parquet(file_path)
             logger.info(f"Successfully read Parquet file: {file_path}")
-        elif file_extension == '.json':
+        elif file_extension == ".json":
             df = pd.read_json(file_path)
             logger.info(f"Successfully read JSON file: {file_path}")
-        elif file_extension == '.csv':
+        elif file_extension == ".csv":
             df = pd.read_csv(file_path)
             logger.info(f"Successfully read CSV file: {file_path}")
         else:
             logger.info(f"Unsupported file format: {file_extension}")
             return None
         
-        return df.to_dict(orient='records')
+        return df.to_dict(orient="records")
     except Exception as e:
         logger.error(f"Error reading file: {e}")
         return None
@@ -48,25 +48,25 @@ def save_scraped_data(data, filename, season, format):
             raise ValueError("Data must be a dictionary or a list.")
 
         df = pd.DataFrame(data)
-        for column in df.select_dtypes(exclude=['number', 'datetime']).columns:
-            df[column] = df[column].astype('string')
+        for column in df.select_dtypes(exclude=["number", "datetime"]).columns:
+            df[column] = df[column].astype("string")
         if season:
             filename = f"{season}_{filename}"
 
-        if format == 'parquet':
+        if format == "parquet":
             file_path = os.path.join(OUTPUT_DIR, f"{filename}.parquet")
             df.to_parquet(file_path, engine="pyarrow", index=False)
 
             logger.info(f"Successfully save Parquet file: {file_path}")
-        elif format == 'json':
+        elif format == "json":
             file_path = os.path.join(OUTPUT_DIR, f"{filename}.json")
             json_data = df.to_json(orient="records", indent=4, force_ascii=False)
 
-            json_data = json_data.replace(r'\/', '/')
-            with open(file_path, 'w', encoding='utf-8') as f:
+            json_data = json_data.replace(r"\/", "/")
+            with open(file_path, "w", encoding="utf-8") as f:
                 f.write(json_data)
             logger.info(f"Successfully save JSON file: {file_path}")
-        elif format == 'csv':
+        elif format == "csv":
             file_path = os.path.join(OUTPUT_DIR, f"{filename}.csv")
             df.to_csv(file_path, index=False, encoding="utf-8") 
 
