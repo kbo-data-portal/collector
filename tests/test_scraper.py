bc2e1c5fa36a5e26d471f1203ca3b23ac36fa216
Rooam Lee <leewr9@naver.com>
2025-05-09 10:45:36 +0900

    tests:: fix column

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -20,7 +20,12 @@ def test_schedule(test_season, test_date):
     This test ensures:
     1. The scraped schedule data matches the expected columns for the given date.
     """
-    column = ["LE_ID", "SR_ID", "SEASON_ID", "G_DT", "G_DT_TXT", "G_ID", "HEADER_NO", "G_TM", "S_NM", "AWAY_ID", "HOME_ID", "AWAY_NM", "HOME_NM", "T_PIT_P_ID", "T_PIT_P_NM", "B_PIT_P_ID", "B_PIT_P_NM", "W_PIT_P_ID", "W_PIT_P_NM", "SV_PIT_P_ID", "SV_PIT_P_NM", "L_PIT_P_ID", "L_PIT_P_NM", "T_D_PIT_P_ID", "T_D_PIT_P_NM", "B_D_PIT_P_ID", "B_D_PIT_P_NM", "GAME_STATE_SC", "CANCEL_SC_ID", "CANCEL_SC_NM", "GAME_INN_NO", "GAME_TB_SC", "GAME_TB_SC_NM", "GAME_RESULT_CK", "T_SCORE_CN", "B_SCORE_CN", "TV_IF", "VS_GAME_CN", "STRIKE_CN", "BALL_CN", "OUT_CN", "B1_BAT_ORDER_NO", "B2_BAT_ORDER_NO", "B3_BAT_ORDER_NO", "T_P_ID", "T_P_NM", "B_P_ID", "B_P_NM", "GAME_SC_ID", "GAME_SC_NM", "IE_ENTRY_CK", "START_PIT_CK", "T_GROUP_SC", "T_RANK_NO", "B_GROUP_SC", "B_RANK_NO", "ROUND_SC", "DETAIL_SC", "GAME_NO", "LINEUP_CK", "VOD_CK", "KBOT_SE", "SCORE_CK", "CHECK_SWING_CK"]
+    column = ["LE_ID", "SR_ID", "SEASON_ID", "G_DT", "G_DT_TXT", "G_ID", "HEADER_NO", "G_TM", "S_NM", "AWAY_ID", "HOME_ID", "AWAY_NM", "HOME_NM", 
+              "T_PIT_P_ID", "T_PIT_P_NM", "B_PIT_P_ID", "B_PIT_P_NM", "W_PIT_P_ID", "W_PIT_P_NM", "SV_PIT_P_ID", "SV_PIT_P_NM", "L_PIT_P_ID", "L_PIT_P_NM", 
+              "T_D_PIT_P_ID", "T_D_PIT_P_NM", "B_D_PIT_P_ID", "B_D_PIT_P_NM", "GAME_STATE_SC", "CANCEL_SC_ID", "CANCEL_SC_NM", "GAME_INN_NO", "GAME_TB_SC", "GAME_TB_SC_NM", "GAME_RESULT_CK", 
+              "T_SCORE_CN", "B_SCORE_CN", "TV_IF", "VS_GAME_CN", "STRIKE_CN", "BALL_CN", "OUT_CN", "B1_BAT_ORDER_NO", "B2_BAT_ORDER_NO", "B3_BAT_ORDER_NO", 
+              "T_P_ID", "T_P_NM", "B_P_ID", "B_P_NM", "GAME_SC_ID", "GAME_SC_NM", "IE_ENTRY_CK", "START_PIT_CK", "T_GROUP_SC", "T_RANK_NO", "B_GROUP_SC", "B_RANK_NO", 
+              "ROUND_SC", "DETAIL_SC", "GAME_NO", "LINEUP_CK", "VOD_CK", "KBOT_SE", "SCORE_CK", "CHECK_SWING_CK"]
     _test_scraper(GameScheduleScraper(), test_season, test_date, column)
 
 
@@ -32,7 +37,9 @@ def test_game(test_season, test_date):
     1. Game details: Ensures the scraped data from the game detail endpoint matches expected columns.
     2. Game statistics: Verifies that both hitter and pitcher statistics contain the correct columns.
     """
-    column = ["IS_HOME", "LE_ID", "SR_ID", "G_ID", "G_DT", "SEASON_ID", "HOME_NM", "HOME_ID", "AWAY_NM", "AWAY_ID", "S_NM", "CROWD_CN", "H_W_CN", "H_L_CN", "H_D_CN", "A_W_CN", "A_L_CN", "A_D_CN", "T_SCORE_CN", "B_SCORE_CN", "START_TM", "END_TM", "USE_TM", "FULL_HOME_NM", "FULL_AWAY_NM", "INN_1", "INN_2", "INN_3", "INN_4", "INN_5", "INN_6", "INN_7", "INN_8", "INN_9", "INN_10", "INN_11", "INN_12", "INN_13", "INN_14", "INN_15", "R", "H", "E", "B"]
+    column = ["IS_HOME", "LE_ID", "SR_ID", "G_ID", "G_DT", "SEASON_ID", "HOME_NM", "HOME_ID", "AWAY_NM", "AWAY_ID", "S_NM", "CROWD_CN", 
+              "H_W_CN", "H_L_CN", "H_D_CN", "A_W_CN", "A_L_CN", "A_D_CN", "T_SCORE_CN", "B_SCORE_CN", "START_TM", "END_TM", "USE_TM", "FULL_HOME_NM", "FULL_AWAY_NM", 
+              "INN_1", "INN_2", "INN_3", "INN_4", "INN_5", "INN_6", "INN_7", "INN_8", "INN_9", "INN_10", "INN_11", "INN_12", "INN_13", "INN_14", "INN_15", "R", "H", "E", "B"]
     _test_scraper(GameResultScraper(), test_season, test_date, column)
 
 
